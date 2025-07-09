5c404466a9e9690f16ae88133bf4159c04634b4d
hejunhao8 <jh2265339886@gmail.com>
2025-07-09 15:51:22 +0800

    删除菜品业务代码

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,9 +1,13 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface DishService {
 
@@ -11,6 +15,14 @@ public interface DishService {
      * 新增菜品和对应口味数据
      * @param dishDTO
      */
-
     public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分类查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    void deleteBatch(List<Long> ids);
 }
